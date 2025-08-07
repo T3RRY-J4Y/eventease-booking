67512d27d845d52adea13dd7dfa378909b9943dc
Siyabonga Terry-Jnr Maluleke <tjay6898@gmail.com>
2025-08-07 20:10:08 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using EventEase.Web.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
