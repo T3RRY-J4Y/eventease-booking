10b287575ff44966a05a293a18aee032ac8d6a08
Siyabonga Terry-Jnr Maluleke <tjay6898@gmail.com>
2025-08-23 03:03:08 +0200

    Update

--- obj/Debug/net8.0/EventEase.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEase.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e13b3046c82e2b8ee662cfcddfd7cd7550359a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671b3a393a22f3f8b3184576f05da9df083c5422")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
